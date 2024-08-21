fa6f58c65d6e40276336106c9b718be8ed42bcfe
alanmd542 <alandelucam@gmail.com>
2024-08-21 15:08:51 -0300

    Add files via upload

---
+++ envio mensaje.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recoge y limpia los datos del formulario
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Dirección de correo electrónico del destinatario
+    $to = 'delucaalanmauricio@gmail.com'; // Cambia esto a tu dirección de correo
+    $subject = 'Nuevo mensaje de contacto de ' . $name;
+    
+    // Cuerpo del mensaje
+    $body = "Nombre: $name\n";
+    $body .= "Correo Electrónico: $email\n";
+    $body .= "Mensaje:\n$message\n";
+
+    // Encabezados del correo
+    $headers = "From: $email" . "\r\n" .
+            "Reply-To: $email" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
+
+    // Envía el correo electrónico
+    if (mail($to, $subject, $body, $headers)) {
+        echo '<p>Mensaje enviado exitosamente.</p>';
+    } else {
+        echo '<p>Error al enviar el mensaje. Inténtalo de nuevo.</p>';
+    }
+} else {
+    // Redirige si no es una solicitud POST
+    header("Location: index.php");
+    exit();
+}
+?>
