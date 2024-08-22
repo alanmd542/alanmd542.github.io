9d2389ca609133959c95d5002783032935b2e681
alanmd542 <alandelucam@gmail.com>
2024-08-22 14:20:52 -0300

    Update envio mensaje.php

--- envio mensaje.php
+++ envio mensaje.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recoge y limpia los datos del formulario
     $name = htmlspecialchars($_POST['name']);
